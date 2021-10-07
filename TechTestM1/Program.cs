70e8fd4bbfcdc6890533827fe1ccf5fe585cf22a
Joao Luz <joaotest85@gmail.com>
2021-10-07 10:31:54 +0100

    Add project files.

---
+++ TechTestM1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechTestM1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
